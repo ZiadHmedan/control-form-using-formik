fb865ac52fd0faa9dc9a2ea6c84bd437601627ed
Zeyad Hmedan <zido.2022@gmail.com>
2024-07-24 11:53:02 +0300

    finalStyleToutch

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -3,10 +3,12 @@ import theme from "../theme";
 
 export const Container = styled.div`
   max-width: 763px;
-  
+
   padding: 4rem;
   background-color: ${theme.colors.white};
   margin: 0 auto;
   border-radius: 10px;
- 
+  @media (max-width: 440px) {
+    margin: 0 16px;
+  }
 `;
